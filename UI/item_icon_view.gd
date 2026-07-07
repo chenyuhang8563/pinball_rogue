9927d1c4eb1ebb803ac74b638bd5a0527a71e523
chenyuhang8563 <2235061910@st.usst.edu.cn>
2026-07-08 02:40:57 +0800

    UI

---
+++ UI/item_icon_view.gd
@@ -0,0 +1,60 @@
+extends Control
+class_name ItemIconView
+
+const LevelBadgeScript: GDScript = preload("res://UI/level_badge.gd")
+
+var _icon: TextureRect
+var _level_badge: Label
+var _level: int = 0
+
+
+func _ready() -> void:
+	_bind_nodes()
+	set_level(_level)
+
+
+func set_texture(texture: Texture2D) -> void:
+	_bind_nodes()
+	if _icon == null:
+		return
+	_icon.texture = texture
+	_icon.visible = texture != null
+
+
+func get_texture() -> Texture2D:
+	_bind_nodes()
+	if _icon == null:
+		return null
+	return _icon.texture
+
+
+func set_level(level: int) -> void:
+	_bind_nodes()
+	_level = level
+	if _level_badge == null:
+		return
+	if level <= 0:
+		_level_badge.text = ""
+		_level_badge.hide()
+		return
+	LevelBadgeScript.apply_to_label(_level_badge, level)
+	_level_badge.show()
+
+
+func set_level_visible(enabled: bool) -> void:
+	_bind_nodes()
+	if _level_badge == null:
+		return
+	_level_badge.visible = enabled and _level > 0
+
+
+func clear() -> void:
+	set_texture(null)
+	set_level(0)
+
+
+func _bind_nodes() -> void:
+	if _icon != null and _level_badge != null:
+		return
+	_icon = get_node_or_null("Icon") as TextureRect
+	_level_badge = get_node_or_null("LevelBadge") as Label
